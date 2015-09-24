f87d703a5ab7849249ee8e5f01f7c45d1fcd2ad2
Asier Iturralde Sarasola <asier.iturralde@gmail.com>
2015-09-24 21:49:56 +0200

    Create getAliveCellCount method

--- js/CellularAutomaton.js
+++ js/CellularAutomaton.js
@@ -43,61 +43,69 @@ CellularAutomaton.prototype.drawRandomValues = function(ctx) {
 
 };
 
+CellularAutomaton.prototype.getAliveCellCount = function(x, y) {
+
+    var alive_cells = 0;
+
+    // Count surrounding alive cells.
+
+    // Top left
+    if (x > 0 && y > 0 && this.cells[y - 1][x - 1] === true) {
+        alive_cells++;
+    }
+
+    // Top
+    if (y > 0 && this.cells[y - 1][x] === true) {
+        alive_cells++;
+    }
+
+    // Top right
+    if (x < this.width - 1 && y > 0 && this.cells[y - 1][x + 1] === true) {
+        alive_cells++;
+    }
+
+    // Left
+    if (x > 0 && this.cells[y][x - 1] === true) {
+        alive_cells++;
+    }
+
+    // Right
+    if (x < this.width -1 && this.cells[y][x + 1] === true) {
+        alive_cells++;
+    }
+
+    // Bottom left
+    if (x > 0 && y < this.height -1 && this.cells[y + 1][x - 1] === true) {
+        alive_cells++;
+    }
+
+    // Bottom
+    if (y < this.height - 1 && this.cells[y + 1][x] === true) {
+        alive_cells++;
+    }
+
+    // Bottom right
+    if (x < this.width - 1 && y < this.height - 1 && this.cells[y + 1][x + 1] === true) {
+        alive_cells++;
+    }
+
+    return alive_cells;
+
+};
+
 CellularAutomaton.prototype.drawNextStep = function(ctx) {
 
-    var living_cells = 0;
+    var alive_cells = 0;
 
     for (var y = 0; y < this.height; y++) {
 
         for (var x = 0; x < this.width; x++) {
 
-            living_cells = 0;
-
-            // Count sourrounding livingthis.cells.
-
-            // Top left
-            if (x > 0 && y > 0 && this.cells[y - 1][x - 1] === true) {
-                living_cells++;
-            }
-
-            // Top
-            if (y > 0 && this.cells[y - 1][x] === true) {
-                living_cells++;
-            }
-
-            // Top right
-            if (x < this.width - 1 && y > 0 && this.cells[y - 1][x + 1] === true) {
-                living_cells++;
-            }
-
-            // Left
-            if (x > 0 && this.cells[y][x - 1] === true) {
-                living_cells++;
-            }
-
-            // Right
-            if (x < this.width -1 && this.cells[y][x + 1] === true) {
-                living_cells++;
-            }
-
-            // Bottom left
-            if (x > 0 && y < this.height -1 && this.cells[y + 1][x - 1] === true) {
-                living_cells++;
-            }
-
-            // Bottom
-            if (y < this.height - 1 && this.cells[y + 1][x] === true) {
-                living_cells++;
-            }
-
-            // Bottom right
-            if (x < this.width - 1 && y < this.height - 1 && this.cells[y + 1][x + 1] === true) {
-                living_cells++;
-            }
+            alive_cells = this.getAliveCellCount(x, y);
 
             for (var i = 0; i < this.rules.born.length; i++) {
 
-                if (this.rules.born[i] === true && i === living_cells) {
+                if (this.rules.born[i] === true && i === alive_cells) {
 
                     this.cells[y][x] = true;
 
@@ -111,7 +119,7 @@ CellularAutomaton.prototype.drawNextStep = function(ctx) {
 
             for (var j = 0; j < this.rules.survive.length; j++) {
 
-                if (this.rules.survive[j] === false && j === living_cells) {
+                if (this.rules.survive[j] === false && j === alive_cells) {
 
                     this.cells[y][x] = false;
 
