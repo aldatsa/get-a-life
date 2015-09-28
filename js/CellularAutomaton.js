f0d5834a33134733cd7c001010a56dd1b1fb12ff
Asier Iturralde Sarasola <asier.iturralde@gmail.com>
2015-09-28 23:37:01 +0200

    Clean debugging stuff

--- js/CellularAutomaton.js
+++ js/CellularAutomaton.js
@@ -85,17 +85,11 @@ CellularAutomaton.prototype.initialize = function(ctx, type) {
             //  xx
             // xx
             //  x
-            /*this.cells[0][this.height / 2][this.width / 2] = true;
+            this.cells[0][this.height / 2][this.width / 2] = true;
             this.cells[0][this.height / 2][this.width / 2 + 1] = true;
             this.cells[0][this.height / 2 + 1][this.width / 2 - 1] = true;
             this.cells[0][this.height / 2 + 1][this.width / 2] = true;
             this.cells[0][this.height / 2 + 2][this.width / 2] = true;
-*/
-            this.cells[0][1][2] = true;
-            this.cells[0][1][3] = true;
-            this.cells[0][2][1] = true;
-            this.cells[0][2][2] = true;
-            this.cells[0][3][2] = true;
 
             break;
 
@@ -132,17 +126,6 @@ CellularAutomaton.prototype.initialize = function(ctx, type) {
             break;
 
     }
-
-    console.log(this.cells[0][0]);
-    console.log(this.cells[0][1]);
-    console.log(this.cells[0][2]);
-    console.log(this.cells[0][3]);
-    console.log(this.cells[0][4]);
-    console.log(this.cells[0][5]);
-    console.log(this.cells[0][6]);
-    console.log(this.cells[0][7]);
-    console.log(this.cells[0][8]);
-    console.log(this.cells[0][9]);
 
 };
 
@@ -182,52 +165,52 @@ CellularAutomaton.prototype.getAliveCellCount = function(x, y, index) {
 
     // Top left
     if (x > 0 && y > 0 && this.cells[index][y - 1][x - 1] === true) {
-        console.log("tl");
+
         alive_cells++;
     }
 
     // Top
     if (y > 0 && this.cells[index][y - 1][x] === true) {
-        console.log("t");
+
         alive_cells++;
     }
 
     // Top right
     if (x < this.width - 1 && y > 0 && this.cells[index][y - 1][x + 1] === true) {
-        console.log("tr");
+
         alive_cells++;
     }
 
     // Left
     if (x > 0 && this.cells[index][y][x - 1] === true) {
-        console.log("l");
+
         alive_cells++;
     }
 
     // Right
     if (x < this.width -1 && this.cells[index][y][x + 1] === true) {
-        console.log("r");
+
         alive_cells++;
     }
 
     // Bottom left
     if (x > 0 && y < this.height -1 && this.cells[index][y + 1][x - 1] === true) {
-        console.log("bl");
+
         alive_cells++;
     }
 
     // Bottom
     if (y < this.height - 1 && this.cells[index][y + 1][x] === true) {
-        console.log("b");
+
         alive_cells++;
     }
 
     // Bottom right
     if (x < this.width - 1 && y < this.height - 1 && this.cells[index][y + 1][x + 1] === true) {
-        console.log("br");
+
         alive_cells++;
     }
-    console.log("---");
+
     return alive_cells;
 
 };
@@ -250,7 +233,7 @@ CellularAutomaton.prototype.calculateNextGeneration = function(ctx) {
     // this.cells[this.current_array_index] = this.cells[this.previous_array_index].slice(0);
     // We must deep copy: http://james.padolsey.com/javascript/deep-copying-of-objects-and-arrays/
     this.cells[this.current_array_index] = this.deepCopy(this.cells[this.previous_array_index]);
-    
+
     for (var y = 0; y < this.height; y++) {
 
         for (var x = 0; x < this.width; x++) {
@@ -263,7 +246,7 @@ CellularAutomaton.prototype.calculateNextGeneration = function(ctx) {
                 for (var i = 0; i < this.rules.born.length; i++) {
 
                     if (this.rules.born[i] === true && i === alive_cells) {
-                        console.log(y + " " + x + " borns");
+
                         this.cells[this.current_array_index][y][x] = true;
                         break;
                     }
@@ -273,12 +256,10 @@ CellularAutomaton.prototype.calculateNextGeneration = function(ctx) {
             // The current cell is alive. Does it survive?
             } else {
 
-                console.log(y + " " + x + " has " + alive_cells + " alive neighbors");
-
                 for (var j = 0; j < this.rules.survive.length; j++) {
 
                     if (this.rules.survive[j] === false && j === alive_cells) {
-                        console.log(y + " " + x + " dies");
+
                         this.cells[this.current_array_index][y][x] = false;
 
                         break;
@@ -288,16 +269,4 @@ CellularAutomaton.prototype.calculateNextGeneration = function(ctx) {
             }
         }
     }
-
-    console.log(this.cells[this.current_array_index][0]);
-    console.log(this.cells[this.current_array_index][1]);
-    console.log(this.cells[this.current_array_index][2]);
-    console.log(this.cells[this.current_array_index][3]);
-    console.log(this.cells[this.current_array_index][4]);
-    console.log(this.cells[this.current_array_index][5]);
-    console.log(this.cells[this.current_array_index][6]);
-    console.log(this.cells[this.current_array_index][7]);
-    console.log(this.cells[this.current_array_index][8]);
-    console.log(this.cells[this.current_array_index][9]);
-
 };
